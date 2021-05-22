bfe2daff2386a3acf2f434476ea34f8a611bc6cd
Kealy Doyle <kealyd@gmail.com>
2021-05-22 13:18:15 -0700

    Save Mom's phone number and last called  time

--- App.js
+++ App.js
@@ -1,11 +1,77 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Linking,
+  TouchableOpacity,
+  TextInput,
+  Button,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const call = (phoneNumber) => Linking.openURL(`tel:${phoneNumber}`);
+
+// track when user clicks call your mom.
+// display the last time user clicked call your mom.
+
+// "lastTimeUserCalledMom"
 
 export default function App() {
+  const [phone, setPhone] = useState("");
+  const [nav, setNav] = useState("settings");
+  const [lastCalledTime, setLastCalledTime] = useState();
+
+  const saveAndNav = () => {
+    setNav("Home");
+    AsyncStorage.setItem("phoneNumber", phone);
+  };
+
+  const track = () => {
+    // record current datetime in asyncstorage
+    const currentTime = new Date().toString();
+    console.log("setting last called time", currentTime);
+    AsyncStorage.setItem("lastCalledTime", currentTime);
+    setLastCalledTime(currentTime);
+  };
+
+  const callAndTrack = () => {
+    const phoneNumber = AsyncStorage.getItem("phoneNumber");
+    call(phoneNumber);
+    track();
+  };
+
+  useEffect(async () => {
+    let time = await AsyncStorage.getItem("lastCalledTime");
+    console.log(time);
+    setLastCalledTime(time);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Call you mom!</Text>
+      {nav === "settings" ? (
+        <View>
+          <Text>Settings</Text>
+          <TextInput
+            onChangeText={setPhone}
+            value={phone}
+            style={styles.input}
+          />
+          <Button title={"SAVE"} onPress={() => saveAndNav()} />
+        </View>
+      ) : (
+        <>
+          <Text>
+            {lastCalledTime
+              ? `You last called your mom at ${lastCalledTime}`
+              : "You haven't called your mom"}
+          </Text>
+          <TouchableOpacity onPress={() => callAndTrack()}>
+            <Text>Call you momma!</Text>
+          </TouchableOpacity>
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +80,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 100,
+    backgroundColor: "lightgray",
   },
 });
---
+++ Settings.js
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Linking,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
+export default function Settings() {}
+
+const styles = StyleSheet.create({});
